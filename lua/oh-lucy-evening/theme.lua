7e908ea73672813e8f17672c1a90e0b0d15a2b2a
Yazeed1s <106896327+Yazeed1s@users.noreply.github.com>
2022-11-07 19:42:24 -0500

    fixed #5

--- lua/oh-lucy-evening/theme.lua
+++ lua/oh-lucy-evening/theme.lua
@@ -16,7 +16,7 @@ M.base = {
     -----------------------------------------
     --        Editors settings
     -----------------------------------------
-    Boolean = { fg = colors.orange },
+    Boolean = { fg = colors.boolean , style = config.italic_booleans},
 
     Character    = { fg = colors.yellow },
     ColorColumn  = { bg = colors.black1 },
@@ -50,7 +50,7 @@ M.base = {
 
     Float       = { fg = colors.orange },
     FloatBorder = { fg = colors.gray2, bg = "NONE" },
-    FoldColumn  = { fg = colors.line_fg },
+    FoldColumn  = { fg = colors.line_fg, bg = colors.bg },
     Folded      = { fg = colors.white, bg = colors.gray },
     Function    = { fg = colors.green_func },
 
@@ -64,7 +64,7 @@ M.base = {
     Label  = { fg = colors.red_key_w },
     LineNr = { fg = colors.line_fg, bg = colors.line_bg },
 
-    Macro         = { fg = colors.red_key_w },
+    Macro         = { fg = colors.blue_type },
     MatchParen    = { fg = colors.white1, bg = colors.black },
     MatchParenCur = { style = 'underline' },
     MatchWord     = { style = 'underline' },
@@ -109,7 +109,7 @@ M.base = {
     StatusLineSeparator = { fg = colors.dark },
     StatusLineTerm      = { fg = colors.green_func, bg = colors.black },
     StatusLineTermNC    = { fg = colors.gray_punc, bg = colors.black },
-    StorageClass        = { fg = colors.red_key_w },
+    StorageClass        = { fg = colors.blue_type },
     String              = { fg = colors.yellow },
     Structure           = { fg = colors.green_func },
     Substitute          = { fg = colors.gray2, bg = colors.orange },
@@ -192,6 +192,17 @@ M.plugins = {
     BufferVisibleSign    = { fg = colors.green, bg = colors.bg },
     BufferVisibleTarget  = { fg = colors.red_key_w, bg = colors.bg, style = 'bold' },
     BufferLineFill       = { fg = colors.fg, bg = colors.gray, style = 'bold' },
+    BufferLineSeparator  = { fg = colors.fg, bg = colors.bg },
+    BufferLineTab  = { fg = colors.fg, bg = colors.bg },
+    BufferLineModifiedVisible = { fg = colors.fg, bg = colors.bg },
+    BufferLineDuplicate = { fg = colors.fg, bg = colors.bg },
+    BufferLineDuplicateVisible = { fg = colors.fg, bg = colors.bg },
+    BufferLineCloseButtonVisible  = { fg = colors.fg, bg = colors.bg },
+    BufferLineCloseButton  = { fg = colors.fg, bg = colors.bg },
+    BufferLineModified  = { fg = colors.fg, bg = colors.bg },
+    BufferLineBufferVisible      = { fg = colors.fg, bg = colors.bg },
+    BufferLineBackground  = { fg = colors.fg, bg = colors.bg },
+
     -----------------------------------------
 
 
@@ -551,7 +562,7 @@ M.plugins = {
     -----------------------------------------
     TSAnnotation         = { fg = colors.yellow },
     TSAttribute          = { fg = colors.white },
-    TSBoolean            = { fg = colors.pink },
+    TSBoolean            = { fg = colors.pink, style = config.italic_booleans },
     TSCharacter          = { fg = colors.yellow },
     TSCharacterSpecial   = { fg = colors.yellow },
     TSComment            = { fg = colors.comment, style = 'italic' },
