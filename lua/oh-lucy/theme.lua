16d77048f456a7aea6e6b1c2f2eff1c2e064be9d
VKTRenokh <viktorenokh@gmail.com>
2025-02-11 13:50:07 +0200

    feat: add support for rainbow-delimiters.nvim

--- lua/oh-lucy/theme.lua
+++ lua/oh-lucy/theme.lua
@@ -172,6 +172,16 @@ M.base = {
 
 
 M.plugins = {
+    -----------------------------------------
+    --   Rainbow delimiters: github.com/hiphish/rainbow-delimiters.nvim
+    -----------------------------------------
+    RainbowDelimiterRed = { fg = colors.red_key_w },
+    RainbowDelimiterOrange = { fg = colors.orange },
+    RainbowDelimiterYellow = { fg = colors.yellow },
+    RainbowDelimiterGreen = { fg = colors.green },
+    RainbowDelimiterBlue = { fg = colors.blue_type },
+    RainbowDelimiterViolet = { fg = colors.pink },
+    RainbowDelimiterCyan = { fg = colors.boolean },
 
     -----------------------------------------
     --   Buffer:
@@ -404,6 +414,10 @@ M.plugins = {
     LspDiagnosticsVirtualTextHint        = { fg = colors.gray2 }, -- Deprecated
     LspDiagnosticsVirtualTextInformation = { fg = colors.yellow }, -- Deprecated
     LspDiagnosticsUnderlineInformation   = { style = 'underline' }, -- Deprecated
+    -----------------------------------------
+
+    -----------------------------------------
+    -- Rainbow delimiters:   github.comhiphish/rainbow-delimiters.nvim/
     -----------------------------------------
 
 
