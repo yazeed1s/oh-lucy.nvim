ec2ccb06c528ba26c13f3c60d6c69d239cc0397c
Kaikaci <lukakuka735@gmail.com>
2025-03-24 20:22:18 +0400

    fix colors_name not being set

--- lua/oh-lucy/init.lua
+++ lua/oh-lucy/init.lua
@@ -2,6 +2,7 @@ local util          = require 'oh-lucy.util'
 local theme         = require 'oh-lucy.theme'
 
 vim.o.background    = 'dark'
-vim.g.colors_name   = 'oh-lucy'
 
 util.load(theme)
+
+vim.g.colors_name   = 'oh-lucy'
